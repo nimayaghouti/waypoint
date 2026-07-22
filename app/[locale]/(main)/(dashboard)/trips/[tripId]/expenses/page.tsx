29641a510ae5fca6ec96610c5845656cfde0dff2
nima.yaghouti <nima.yaghouti99@gmail.com>
2026-07-22 15:50:20 +0330

    feat: add placeholder pages and components for trip dashboard page

---
+++ app/[locale]/(main)/(dashboard)/trips/[tripId]/expenses/page.tsx
@@ -0,0 +1,3 @@
+export default function TripExpensesPage() {
+  return <div className="text-xl font-bold">Expenses Module (Coming Soon)</div>;
+}
---
+++ app/[locale]/(main)/(dashboard)/trips/[tripId]/itinerary/page.tsx
@@ -0,0 +1,5 @@
+export default function TripItineraryPage() {
+  return (
+    <div className="text-xl font-bold">Itinerary Module (Coming Soon)</div>
+  );
+}
